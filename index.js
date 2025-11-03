3d3caf7fc8f9cbc662a1ba5698e9b97af47d2554
EllaWiesel <ell.wiesel.24@lehre.dhbw.mosbach.de>
2025-11-03 15:03:40 +0100

    back to old logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*require('dotenv').config();
+require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
 const express = require('express');
@@ -67,64 +67,4 @@ const loginRes = await axios.post(
   }
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);*/
-
-const userTokens = new Map();
-
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-
-  const args = message.content.split(' ');
-
-  if (message.content.startsWith('!login')) {
-    const username = args[1];
-    const password = args[2];
-
-    if (!username || !password) {
-      message.reply('Bitte nutze: `!login <username> <password>`');
-      return;
-    }
-
-    try {
-      const loginRes = await axios.post(
-        'https://bff-webprogrammierung-6322597a0426.herokuapp.com/api/login?username='+ username + '&password=' + password,
-        { username, password },
-        { headers: { 'Content-Type': 'application/json' } }
-      );
-
-      const token = loginRes.data.token;
-      userTokens.set(message.author.id, token); // Speichere Token für den User
-
-      message.reply('Login erfolgreich! Du kannst jetzt `!wishlist` verwenden.');
-    } catch (err) {
-      console.error(err);
-      message.reply('Login fehlgeschlagen.');
-    }
-  }
-
-  if (message.content.startsWith('!wishlist')) {
-    const token = userTokens.get(message.author.id);
-
-    if (!token) {
-      message.reply('❗ Du musst dich zuerst mit `!login <username> <password>` einloggen.');
-      return;
-    }
-
-    try {
-      const productsRes = await axios.get(
-        'https://bff-webprogrammierung-6322597a0426.herokuapp.com/api/wishlist?token='+ token,
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-
-      const products = productsRes.data;
-      let count = 1;
-      for (const product of products) {
-        await message.channel.send(`${count}: ${product.title} with ${product.price} €`);
-        count++;
-      }
-    } catch (err) {
-      console.error(err);
-      message.reply('Fehler beim Abrufen der Produkte.');
-    }
-  }
-});
+client.login(process.env.DISCORD_BOT_TOKEN);
