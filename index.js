ae21bbe10d3cc6e7a85a926cd77688bf33052356
EllaWiesel <ell.wiesel.24@lehre.dhbw.mosbach.de>
2025-11-03 14:12:13 +0100

    init files

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+const express = require('express');
+
+// Express-Server für Heroku "Keep Alive"
+const app = express();
+app.get('/', (req, res) => res.send('Bot läuft!'));
+app.listen(process.env.PORT || 3000);
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on('ready', () => {
+  console.log(`Bot ist online als ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith('!login')) {
+    const args = message.content.split(' ');
+    const username = args[1];
+    const password = args[2];
+
+    if (!username || !password) {
+      message.reply('Bitte nutze: `!login <username> <password>`');
+      return;
+    }
+
+    try {
+      
+        const loginRes = await axios.post('https://bff-webprogrammierung-6322597a0426.herokuapp.com/api/login?username=' + username + '&password=' + password);
+
+        const token = loginRes.data.token;
+
+      const productsRes = await axios.get('https://bff-webprogrammierung-6322597a0426.herokuapp.com/api/wishlist?token=' + token);
+
+      const products = productsRes.data;
+
+      for (const product of products) {
+        await message.channel.send(`${product.title} with ${product.price} €`);
+      }
+
+    } catch (err) {
+      console.error(err);
+      message.reply('Fehler beim Login oder beim Abrufen der Produkte.');
+    }
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
